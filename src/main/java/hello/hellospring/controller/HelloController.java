ea712d5cd80862e571bd2903c4bc34457ebf912b
[lee0119] <[candogo91@gmail.com]>
2022-08-25 00:09:50 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,10 @@
+package hello.hellospring.controller;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+}
